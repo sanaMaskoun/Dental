372c1f7a640ff2bdcb094c84ebda89d3d047d285
sana mohammed <sana.maskoun2001@gmail.com>
2025-04-12 00:52:25 +0300

    final

--- lang/tr/message.php
+++ lang/tr/message.php
@@ -11,7 +11,23 @@ return [
 
      'status_updated'           => 'Durum başarıyla güncellendi.',
      'conflict_appointment'     =>'Bir zamanlama çakışması var. Lütfen sonuncuyu seçin.',
-     'add'       =>'Added successfully',
+     'add'       =>'Başarıyla eklendi',
+      'event_updated'             =>'Etkinlik başarıyla güncellendi',
+      'appointment_delete'        => 'Randevu başarıyla silindi.',
+      'completed_successfully' => 'başarıyla tamamlandı',
+      'add_balance'     =>'Bakiye başarıyla eklendi',
+      'reject_balance'  =>'Bakiye reddedildi',
+      'update'         =>'Başarıyla değiştirildi',
+      'delete'         =>'başarıyla sil',
+      'inactive_account'  =>'Değiştirmek istediğiniz hesap etkin değil',
+      'recharge_successfully'  => 'Başarıyla Yeniden Şarj Edin',
+      'booking_create'        =>'Rezervasyon oluşturuldu. Bu hizmet için tam tutar zaten ödendiğinden ödemeye gerek yok.',
+      'no_enough_balance'    =>'Hizmeti rezerve etmek için yeterli bakiyeniz yok.',
+      'paid_all_balance'      =>'Rezervasyon oluşturuldu. Tam tutar ödendiği için ödemeye gerek yok.',
+      'booking_successfully'                   => 'Rezervasyon başarıyla tamamlandı.',
+      'join_successfully'        =>'Katılım başarıyla tamamlandı',
+      'join_reject'          =>'Katılma reddedildi.',
+
 
 
 
